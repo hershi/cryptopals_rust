0492a5794ad33fe38d8c9f4d85a2ddac0af0f5a4
Guy Hershenbaum <hershi@fb.com>
2019-09-07 15:28:34 -0700

    Add encryption utils

---
+++ set_2/utils/src/encryption.rs
@@ -0,0 +1,54 @@
+use openssl::symm::{Cipher, Crypter, Mode, encrypt};
+
+pub fn ecb_encrypt(input: &[u8], key: &[u8]) -> Vec<u8> {
+    let cipher = Cipher::aes_128_ecb();
+    encrypt(
+        cipher,
+        key,
+        None,
+        input).unwrap()
+}
+
+pub fn cbc_encrypt(input: &[u8], key: &[u8], mut iv: Vec<u8>) -> Vec<u8> {
+    let cipher = Cipher::aes_128_ecb();
+    let mut encrypter = Crypter::new(
+        cipher,
+        Mode::Encrypt,
+        key,
+        None).unwrap();
+
+    let mut ciphertext = vec![0; input.len() + cipher.block_size()];
+    let mut pos = 0;
+    for chunk in input.chunks(cipher.block_size()) {
+        let chunk = super::xor(chunk, &iv);
+        let count = encrypter.update(&chunk, &mut ciphertext[pos..]).unwrap();
+        iv = ciphertext[pos..pos+cipher.block_size()].to_vec();
+        pos += count;
+    }
+    pos += encrypter.finalize(&mut ciphertext[pos..]).unwrap();
+    ciphertext.truncate(pos);
+
+    ciphertext.to_vec()
+}
+
+pub fn cbc_decrypt(input: &[u8], key: &[u8], iv: &Vec<u8>) -> Vec<u8> {
+    let cipher = Cipher::aes_128_ecb();
+    let mut decrypter = Crypter::new(
+        cipher,
+        Mode::Decrypt,
+        key,
+        None).unwrap();
+
+    // ATTENTION: We are decrypting without padding! If the input is padded
+    // then this will fail or give odd results because padding bytes would not
+    // be stripped.
+    decrypter.pad(false);
+
+    let mut raw_decrypted_bytes = vec![0; input.len() + cipher.block_size()];
+    let count = decrypter.update(input, &mut raw_decrypted_bytes).unwrap();
+
+    raw_decrypted_bytes.iter().take(count)
+        .zip(iv.iter().chain(input.iter())) // iterator for IV bytes
+        .map(|(b,i)| b^i)
+        .collect::<Vec<u8>>()
+}
--- set_2/utils/src/lib.rs
+++ set_2/utils/src/lib.rs
@@ -3,6 +3,7 @@ extern crate lazy_static;
 
 pub mod english_scoring;
 pub mod encoding;
+pub mod encryption;
 
 pub fn xor(input: &[u8], key: &[u8]) -> Vec<u8> {
     input
@@ -19,5 +20,11 @@ pub fn pad_block(mut input: Vec<u8>, block_size: u8) -> Vec<u8> {
     let padding_needed = block_size - last_block_size;
     input.resize(input.len() + padding_needed, padding_needed as u8);
     input
+}
+
+pub fn random_buffer(size: usize) -> Vec<u8> {
+    std::iter::repeat_with(|| rand::random::<u8>())
+        .take(size)
+        .collect()
 }
 
