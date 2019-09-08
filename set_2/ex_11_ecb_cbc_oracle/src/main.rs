895bf4e0645b8a80f8cef8f27e84733b56501b68
Guy Hershenbaum <hershi@fb.com>
2019-09-08 15:00:26 -0700

    Cleanup code

--- set_2/ex_11_ecb_cbc_oracle/src/main.rs
+++ set_2/ex_11_ecb_cbc_oracle/src/main.rs
@@ -1,13 +1,9 @@
-#[macro_use]
-extern crate lazy_static;
-
 use std::collections::HashMap;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
 use utils::*;
 use utils::encryption::*;
-use utils::english_scoring::*;
 use rand::prelude::*;
 
 const BLOCK_SIZE : usize = 16;
@@ -18,16 +14,6 @@ const IV_SIZE : usize = 16;
 enum EncryptionMode {
     CBC,
     ECB
-}
-
-lazy_static! {
-    pub static ref UNIFORM_DIST: HashMap<u8, f32> = {
-        let mut dist = HashMap::new();
-        for b in 0..255u8 {
-            dist.insert(b, 1f32/255f32);
-        }
-        dist
-    };
 }
 
 fn read_input() -> String {
@@ -66,43 +52,18 @@ fn add_pre_post_fixes(input: &[u8]) -> Vec<u8> {
         .collect()
 }
 
-fn encryption_mode_oracle(encrypted_data: &[u8]) -> EncryptionMode {
-    let is_cbc = is_cbc(encrypted_data);
-    let is_ecb = is_ecb(encrypted_data);
-
-    assert!(is_ecb != is_cbc);
-
-    if (is_ecb) { EncryptionMode::ECB } else { EncryptionMode::CBC}
-}
-
-fn is_cbc(encrypted_data: &[u8]) -> bool {
-    score(&encrypted_data, &UNIFORM_DIST) < 1f32
-}
-
-fn is_ecb(input: &[u8]) -> bool {
+fn predicted_mode(input: &[u8]) -> EncryptionMode {
     let mut freq = HashMap::new();
 
     for chunk in input.chunks(BLOCK_SIZE) {
         *freq.entry(chunk).or_insert(0) += 1
     }
 
-    let res = freq.values().any(|v| *v > 1);
-    res
-}
-
-fn score(input: &[u8], expected_frequencies: &HashMap<u8, f32>) -> f32 {
-    let mut frequencies = get_byte_frequencies(input);
-
-    let length = input.len() as f32;
-    for (_, v) in frequencies.iter_mut() {
-        *v = *v / length;
+    if freq.values().any(|v| *v > 1) {
+        EncryptionMode::ECB
+    } else {
+        EncryptionMode::CBC
     }
-
-    expected_frequencies.iter()
-        .map(|(k,v)| {
-            let freq = frequencies.get(&k).unwrap_or(&0f32);
-            (v - freq).abs() })
-        .sum()
 }
 
 fn main() {
@@ -110,7 +71,7 @@ fn main() {
     let input_bytes = input.as_bytes();
     for i in 0..50 {
         let (encrypted, mode) = encryption_oracle(&input_bytes);
-        let oracle_prediction = encryption_mode_oracle(&encrypted);
+        let oracle_prediction = predicted_mode(&encrypted);
         println!("{}: Prediction correct? {}; {:?}: Oracle says - {:?}",
                  i,
                  oracle_prediction == mode,
