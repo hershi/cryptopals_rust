35452a7938f98ffe7f86d43e8b61916c06a59241
Guy Hershenbaum <hershi@fb.com>
2019-09-10 20:44:35 -0700

    Ex13: Basic version of profile_for - no sanitation

--- set_2/ex_13_ecb_cut_and_paste/src/main.rs
+++ set_2/ex_13_ecb_cut_and_paste/src/main.rs
@@ -1,21 +1,30 @@
 use std::collections::HashMap;
 
-fn parse_key_values(input: &str) -> HashMap<String, String> {
+fn parse_key_values(input: &str) -> HashMap<&str, &str> {
     let mut map = HashMap::new();
     for kv_str in input.split('&') {
         let parts = kv_str.split('=').collect::<Vec<_>>();
         if parts.len() != 2 { continue; }
-        map.insert(parts[0].to_string(), parts[1].to_string());
+        map.insert(parts[0], parts[1]);
     }
 
     map
 }
 
-fn to_key_value_string(kvs: &HashMap<String, String>) -> String {
+fn to_key_value_string(kvs: &HashMap<&str, &str>) -> String {
     kvs.iter()
         .map(|(k,v)| format!("{}={}", k, v))
         .collect::<Vec<_>>()
         .join("&")
+}
+
+fn profile_for(email: &str) -> String {
+    let mut kvs = HashMap::new();
+    kvs.insert("email", email);
+    kvs.insert("uid", "10");
+    kvs.insert("role", "user");
+
+    to_key_value_string(&kvs)
 }
 
 fn main() {
@@ -23,4 +32,7 @@ fn main() {
     let map = parse_key_values(&input);
     println!("Map: {:?}", map);
     println!("As string: {}", to_key_value_string(&map));
+
+    let email = "hello@world.com";
+    println!("profile_for(\"{}\"): {}", email, profile_for(&email));
 }
