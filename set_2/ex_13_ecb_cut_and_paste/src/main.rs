d48cd3890d77d003cdb0c4875a64518edb09c96b
Guy Hershenbaum <hershi@fb.com>
2019-09-10 20:50:03 -0700

    Add (no-op, for now) sanitization func

--- set_2/ex_13_ecb_cut_and_paste/src/main.rs
+++ set_2/ex_13_ecb_cut_and_paste/src/main.rs
@@ -1,28 +1,32 @@
 use std::collections::HashMap;
 
-fn parse_key_values(input: &str) -> HashMap<&str, &str> {
+fn parse_key_values(input: &str) -> HashMap<String, String> {
     let mut map = HashMap::new();
     for kv_str in input.split('&') {
         let parts = kv_str.split('=').collect::<Vec<_>>();
         if parts.len() != 2 { continue; }
-        map.insert(parts[0], parts[1]);
+        map.insert(parts[0].to_string(), parts[1].to_string());
     }
 
     map
 }
 
-fn to_key_value_string(kvs: &HashMap<&str, &str>) -> String {
+fn to_key_value_string(kvs: &HashMap<String, String>) -> String {
     kvs.iter()
         .map(|(k,v)| format!("{}={}", k, v))
         .collect::<Vec<_>>()
         .join("&")
 }
 
+fn sanitize(email: &str) -> String {
+    email.to_string()
+}
+
 fn profile_for(email: &str) -> String {
     let mut kvs = HashMap::new();
-    kvs.insert("email", email);
-    kvs.insert("uid", "10");
-    kvs.insert("role", "user");
+    kvs.insert("email".to_string(), sanitize(email));
+    kvs.insert("uid".to_string(), "10".to_string());
+    kvs.insert("role".to_string(), "user".to_string());
 
     to_key_value_string(&kvs)
 }
@@ -33,6 +37,6 @@ fn main() {
     println!("Map: {:?}", map);
     println!("As string: {}", to_key_value_string(&map));
 
-    let email = "hello@world.com";
+    let email = "email=hello@world.com&a=b";
     println!("profile_for(\"{}\"): {}", email, profile_for(&email));
 }
