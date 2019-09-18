459604141b5644bea28289e67b412ccc8e0357a7
Guy Hershenbaum <hershi@fb.com>
2019-09-17 21:46:17 -0700

    Challenge 16: basic scaffolding ready

--- set_2/ex_16_cbc_bitflipping/src/main.rs
+++ set_2/ex_16_cbc_bitflipping/src/main.rs
@@ -7,12 +7,14 @@ use utils::encryption::*;
 
 const BLOCK_SIZE : usize = 16;
 const KEY_SIZE : usize = 16;
+const IV_SIZE : usize = 16;
 
 const PREFIX : &[u8] = b"comment1=cooking%20MCs;userdata=";
 const SUFFIX : &[u8] = b";comment2=%20like%20a%20pound%20of%20bacon";
 
 lazy_static! {
     pub static ref KEY: Vec<u8> = random_buffer(KEY_SIZE);
+    pub static ref IV: Vec<u8> = random_buffer(IV_SIZE);
 }
 
 fn sanitize(user_data: &[u8]) -> Vec<u8> {
@@ -42,6 +44,23 @@ fn generate_input(user_data: &[u8]) -> Vec<u8> {
     pad_block(input, BLOCK_SIZE as u8)
 }
 
+fn oracle(user_data: &[u8]) -> Vec<u8> {
+    let input = generate_input(user_data);
+    cbc_encrypt(&input, &KEY, IV.clone())
+}
+
+fn validator(encrypted: &[u8]) -> bool {
+    let decrypted = cbc_decrypt(encrypted, &KEY, &IV);
+    let decrypted_string = to_string(&decrypted);
+    println!("Decrypted: {}", decrypted_string);
+
+    decrypted_string.contains(";admin=true;")
+}
+
 fn main() {
     println!("{}", to_string(&generate_input(b";admin=true;")));
+
+    let encrypted = oracle(b";admin=true;");
+
+    println!("Result: {}", validator(&encrypted));
 }
