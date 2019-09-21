9d42ec8f3a26df76e049cde8db2a163e4e37fe8f
Guy Hershenbaum <hershi@fb.com>
2019-09-21 16:12:19 -0700

    Challenge 17 - woerking version

--- ex_17_cbc_padding_oracle/src/main.rs
+++ ex_17_cbc_padding_oracle/src/main.rs
@@ -3,7 +3,9 @@ extern crate lazy_static;
 
 use rand::prelude::*;
 use utils::*;
+use utils::encoding::*;
 use utils::encryption::*;
+use data_encoding::BASE64;
 
 const BLOCK_SIZE : usize = 16;
 const KEY_SIZE : usize = 16;
@@ -14,15 +16,15 @@ lazy_static! {
 
     pub static ref INPUT_STRINGS : Vec<&'static str> = vec![
         "MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=",
-        //"MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=",
-        //"MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==",
-        //"MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==",
-        //"MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl",
-        //"MDAwMDA1SSBnbyBjcmF6eSB3aGVuIEkgaGVhciBhIGN5bWJhbA==",
-        //"MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw==",
-        //"MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8=",
-        //"MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g=",
-        //"MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93",
+        "MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=",
+        "MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==",
+        "MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==",
+        "MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl",
+        "MDAwMDA1SSBnbyBjcmF6eSB3aGVuIEkgaGVhciBhIGN5bWJhbA==",
+        "MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw==",
+        "MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8=",
+        "MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g=",
+        "MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93",
     ];
 }
 
@@ -63,12 +65,10 @@ fn crack_cbc_byte(input_block: &[u8], iv: &[u8], byte_index: usize) -> u8 {
             continue;
         }
 
-        println!("{} Possible: {}", byte_index, b);
         if byte_index == 0 { return b; }
 
         iv[byte_index - 1] ^= 1;
         if !decrypt_and_validate_padding(input_block, &iv) {
-            println!("Previous byte changes - {} is not a good option", b);
             continue;
         }
 
@@ -78,14 +78,25 @@ fn crack_cbc_byte(input_block: &[u8], iv: &[u8], byte_index: usize) -> u8 {
     0
 }
 
-fn prep_iv(iv: &[u8], byte_index: usize, cracked_block: &[u8]) -> Vec<u8> {
-    let padding = (BLOCK_SIZE - byte_index) as u8;
+// Create an IV that would cause the bytes starting at byte_index+1 to decrypt
+// to `desired_value`
+// BLOCK_SIZE - byte_index)
+// To that end, we need to know the decrypted values of those bytes, which
+// we receive in the relative positions in `cracked_block`. To cause them to
+// decrypt to the value we want we just need to figure out which bits need
+// to be flipped (bits_to_flip == desired_value ^ decrypted_byte_value) and then
+// flip those bits in the original IV (== iv_byte ^ bits_to_flip)
+fn prep_iv(
+        iv: &[u8],
+        byte_index: usize,
+        cracked_block: &[u8],
+        desired_value: u8) -> Vec<u8> {
     iv.iter()
         .zip(cracked_block.iter())
         .enumerate()
-        .map(|(i, (&iv_byte,&block_byte))| {
+        .map(|(i, (&iv_byte, &block_byte))| {
             if i > byte_index {
-                iv_byte ^ block_byte ^ padding
+                iv_byte ^ block_byte ^ desired_value
             } else {
                 iv_byte
             }})
@@ -95,20 +106,39 @@ fn prep_iv(iv: &[u8], byte_index: usize, cracked_block: &[u8]) -> Vec<u8> {
 fn crack_cbc_block(input_block: &[u8], iv: &[u8]) -> Vec<u8> {
     let mut cracked_block = vec![0; BLOCK_SIZE];
     for byte_index in 0..BLOCK_SIZE {
+        // We start from the last byte of the block, working our way
+        // backwards
         let byte_index = BLOCK_SIZE - 1 - byte_index;
-        let padding = BLOCK_SIZE - byte_index;
 
-        let iv = prep_iv(iv, byte_index, &cracked_block);
+        // The expected padding value if padding were to start at this byte
+        // equals the number of bytes between here and the end of the block
+        let padding = (BLOCK_SIZE - byte_index) as u8;
+
+        // We want to start with an IV that causes the bytes following the
+        // current byte decrypt to the padding value
+        let iv = prep_iv(iv, byte_index, &cracked_block, padding);
 
         let byte_mask = crack_cbc_byte(input_block, &iv, byte_index);
-        cracked_block[byte_index] = byte_mask ^ padding as u8;
-        println!("Byte {}, {}", byte_index, cracked_block[byte_index]);
+        cracked_block[byte_index] = byte_mask ^ padding;
     }
 
     cracked_block
 }
 
+fn crack_cbc(encrypted_data: &[u8], iv: &[u8]) -> Vec<u8> {
+    std::iter::once(iv)
+        .chain(encrypted_data.chunks(BLOCK_SIZE))
+        .zip(encrypted_data.chunks(BLOCK_SIZE))
+        .map(|(iv, block)| crack_cbc_block(block, iv))
+        .fold(Vec::new(), |mut acc, mut v| { acc.append(&mut v); acc })
+}
+
 fn main() {
     let encrypted = encryption_oracle();
-    crack_cbc_block(encrypted.encrypted_data.chunks(BLOCK_SIZE).nth(0).unwrap(), &encrypted.iv);
+    let block = crack_cbc_block(encrypted.encrypted_data.chunks(BLOCK_SIZE).nth(0).unwrap(), &encrypted.iv);
+    println!("{:?}", to_string(&block));
+    let mut plaintext = crack_cbc(&encrypted.encrypted_data, &encrypted.iv);
+    strip_padding(&mut plaintext, BLOCK_SIZE);
+    println!("{:?}", to_string(&plaintext));
+    println!("{:?}", BASE64.decode(&plaintext).unwrap());
 }
