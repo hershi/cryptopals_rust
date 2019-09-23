7dc08730edd438d243ec62991986afa701529d19
Guy Hershenbaum <hershi@fb.com>
2019-09-22 17:52:04 -0700

    Challenge 18 - fix CTR (misunderstood the original intention)

--- ex_18_ctr/src/main.rs
+++ ex_18_ctr/src/main.rs
@@ -16,34 +16,26 @@ lazy_static! {
     pub static ref KEY: &'static[u8] = "YELLOW SUBMARINE".as_bytes();
 }
 
-fn gen_ctr_counter_bytes(counter: &u128) -> Vec<u8> {
-    counter.to_le_bytes()
-        .chunks(BLOCK_SIZE / 2)
-        .rev()
-        .fold(
-            Vec::with_capacity(BLOCK_SIZE),
-            |mut acc, bytes| { acc.extend_from_slice(bytes); acc })
-}
-
-fn ctr_encrypt(input: &[u8], key: &[u8], iv: &[u8]) -> Vec<u8> {
+fn ctr_encrypt(input: &[u8], key: &[u8], nonce: u64) -> Vec<u8> {
     input.chunks(BLOCK_SIZE)
-        .zip(0u128..)
+        .zip(0u64..)
         .map(|(block, counter)| {
-            let counter_bytes = gen_ctr_counter_bytes(&counter);
-            println!("{:?}", counter_bytes);
-            let key_stream = cbc_encrypt(&counter_bytes, key, iv.to_vec(), false);
+            let mut bytes = nonce.to_le_bytes().to_vec();
+            bytes.extend_from_slice(&counter.to_le_bytes());
+            println!("{:?}", bytes);
+            let key_stream = cbc_encrypt(&bytes, key, IV.to_vec(), false);
             xor(block, &key_stream)})
         .fold(
             Vec::with_capacity(input.len()),
             |mut acc, mut block| { acc.append(&mut block); acc})
 }
 
-fn ctr_decrypt(input: &[u8], key: &[u8], iv: &[u8]) -> Vec<u8> {
-    ctr_encrypt(input, key, iv)
+fn ctr_decrypt(input: &[u8], key: &[u8], nonce: u64) -> Vec<u8> {
+    ctr_encrypt(input, key, nonce)
 }
 
 fn main() {
     let encrypted = BASE64.decode(INPUT.as_bytes()).unwrap();
-    let plaintext = ctr_decrypt(&encrypted, &KEY, &IV);
+    let plaintext = ctr_decrypt(&encrypted, &KEY, 0);
     println!("{}", to_string(&plaintext));
 }
