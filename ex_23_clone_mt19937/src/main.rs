a7750acd903a376a289f0988eaa4a5a0a65b192d
Guy Hershenbaum <hershi@fb.com>
2019-09-27 15:06:16 -0700

    Untemper full - v1

--- ex_23_clone_mt19937/src/main.rs
+++ ex_23_clone_mt19937/src/main.rs
@@ -8,25 +8,43 @@ const C: u32 = 0xEFC60000;
 const L: u32 = 18;
 
 fn temper(y: u32) -> u32 {
-    //let y = y ^ ((y >> U) & D);
-    //let y = y ^ ((y << S) & B);
+    let y = y ^ ((y >> U) & D);
+    let y = y ^ ((y << S) & B);
     let y = y ^ ((y << T) & C);
-    //let y = y ^ (y >> L);
+    let y = y ^ (y >> L);
 
     y
 }
 
-fn untemper_helper1(val: u32, shift: u32, mask: u32) -> u32 {
-    let bitmask = 2u32.pow(shift)-1;
-    let yl = val & bitmask;
-    let yh = (val & (bitmask << shift)) ^ ((yl << shift) & mask);
-    let yhh = (val & (bitmask << (shift *2))) ^ ((yl << (shift *2)) & mask);
-    println!("yl  {:032b}", yl);
-    println!("yh  {:032b}", yh);
-    println!("yhh {:032b}", yhh);
-    println!("cmb {:032b}", yhh | yh | yl);
+fn untemper_right_shift(val: u32, shift: u32, mask: u32) -> u32 {
+    let mut base_bitmask = 2u64.pow(shift)-1 << ((W / shift) * shift);
+    let mut bits_handled = 0;
 
-    yhh | yh | yl
+    let mut acc = 0;
+    while bits_handled < W {
+        let bitmask = base_bitmask as u32;
+        let x = (val & bitmask) ^ (((acc >> shift) & mask) & bitmask);
+        acc ^= x;
+
+        bits_handled += shift;
+        base_bitmask = base_bitmask >> shift;
+    }
+    acc
+}
+
+fn untemper_left_shift(val: u32, shift: u32, mask: u32) -> u32 {
+    let mut bitmask = 2u32.pow(shift)-1;
+    let mut bits_handled = 0;
+
+    let mut acc = 0;
+    while bits_handled < W {
+        let x = (val & bitmask) ^(((acc << shift) & mask) & bitmask);
+        acc ^= x;
+
+        bits_handled += shift;
+        bitmask = bitmask << shift;
+    }
+    acc
 }
 
 
@@ -38,11 +56,11 @@ fn untemper_helper(val: u32, shift: u32) -> u32 {
 }
 
 fn untemper(y: u32) -> u32 {
-    //let y = self.state[self.index];
-    //let y = y ^ ((y >> U) & D);
-    //let y = y ^ ((y << S) & B);
-    //let y = y ^ ((y << T) & C);
-    untemper_helper1(y, T, C)
+    let y = untemper_right_shift(y, L, 0xFFFFFFFF);
+    let y = untemper_left_shift(y, T, C);
+    let y = untemper_left_shift(y, S, B);
+    let y = untemper_right_shift(y, U, D);
+    y
 }
 
 fn main() {
@@ -53,3 +71,4 @@ fn main() {
     println!("val {:032b}", y);
     println!("untemper({}) {}", tempered_y, untempered);
 }
+
