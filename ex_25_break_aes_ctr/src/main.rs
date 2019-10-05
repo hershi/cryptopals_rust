08e235d2d5e89640c0d10e9ec0016423ec42b4d5
Guy Hershenbaum <hershi@fb.com>
2019-10-05 15:47:59 -0700

    Ex 25 solution

--- ex_25_break_aes_ctr/src/main.rs
+++ ex_25_break_aes_ctr/src/main.rs
@@ -5,11 +5,14 @@ use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
 use utils::*;
+use utils::encryption::*;
+use utils::encoding::*;
 use data_encoding::BASE64;
 use openssl::symm::{decrypt, Cipher};
 
 const ECB_KEY : &[u8] = b"YELLOW SUBMARINE";
 const KEY_SIZE : usize = 16;
+const NONCE :u64 = 0;
 
 lazy_static! {
     static ref KEY : Vec<u8> = random_buffer(KEY_SIZE);
@@ -32,6 +35,25 @@ fn read_input() -> Vec<u8> {
         &input).unwrap()
 }
 
+fn edit(ciphertext: &[u8],
+        offset: usize,
+        newtext: &[u8]) -> Vec<u8> {
+
+    let mut plaintext = ctr_decrypt(ciphertext, &KEY, NONCE);
+    plaintext.truncate(offset);
+    plaintext.extend_from_slice(newtext);
+    ctr_encrypt(&plaintext, &KEY, NONCE)
+}
+
 fn main() {
     let plaintext = read_input();
+    let encrypted = ctr_encrypt(&plaintext, &KEY, NONCE);
+
+    let my_text = vec![0u8; encrypted.len()];
+    let edited_ciphertext = edit(&encrypted, 0, &my_text);
+
+    let recovered_plaintext =
+        xor(&xor(&encrypted, &edited_ciphertext), &my_text);
+
+    println!("{}", to_string(&recovered_plaintext));
 }
