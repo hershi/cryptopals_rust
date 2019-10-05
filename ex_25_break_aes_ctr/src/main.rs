3529850ad86e240a8f0d5127831bc888a1157aed
Guy Hershenbaum <hershi@fb.com>
2019-10-05 15:34:18 -0700

    Ex 25 - genearte random key

--- ex_25_break_aes_ctr/src/main.rs
+++ ex_25_break_aes_ctr/src/main.rs
@@ -1,10 +1,19 @@
+#[macro_use]
+extern crate lazy_static;
+
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
+use utils::*;
 use data_encoding::BASE64;
 use openssl::symm::{decrypt, Cipher};
 
 const ECB_KEY : &[u8] = b"YELLOW SUBMARINE";
+const KEY_SIZE : usize = 16;
+
+lazy_static! {
+    static ref KEY : Vec<u8> = random_buffer(KEY_SIZE);
+}
 
 fn read_input() -> Vec<u8> {
     let input_file = File::open("src/input.txt").unwrap();
@@ -24,5 +33,5 @@ fn read_input() -> Vec<u8> {
 }
 
 fn main() {
-    let plaintext = read_input()
+    let plaintext = read_input();
 }
