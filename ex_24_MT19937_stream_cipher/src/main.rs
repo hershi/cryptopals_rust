f9aae82bbcf837c65d1bdd23a2512539aff996db
Guy Hershenbaum <hershi@fb.com>
2019-10-05 09:30:04 -0700

    MT stream encrypt/decrypt/crack

--- ex_24_MT19937_stream_cipher/src/main.rs
+++ ex_24_MT19937_stream_cipher/src/main.rs
@@ -1,8 +1,18 @@
+#[macro_use]
+extern crate lazy_static;
+
 use rand::prelude::*;
 use utils::*;
-use utils::encoding::*;
 use utils::mt19937::*;
 use std::iter::*;
+
+lazy_static! {
+    static ref PREFIX: Vec<u8> = {
+        let mut rng = thread_rng();
+        let prefix_size = rng.gen_range(1, 10000);
+        random_buffer(prefix_size)
+    };
+}
 
 fn mt_encrypt(seed: u16, input:&[u8]) -> Vec<u8> {
     let mut mt = MersenneTwister::new(seed.into());
@@ -18,10 +28,32 @@ fn mt_decrypt(seed: u16, input:&[u8]) -> Vec<u8> {
     mt_encrypt(seed, input)
 }
 
+fn create_input(input: &[u8]) -> Vec<u8> {
+    PREFIX.iter().chain(input.iter()).cloned().collect()
+}
+
+fn crack_seed(encrypted: &[u8], known_plaintext: &[u8]) -> Option<u16> {
+    (u16::min_value()..=u16::max_value())
+        .map(|key| (key, mt_decrypt(key, encrypted)))
+        .find(|(_, decrypted)| decrypted.ends_with(known_plaintext))
+        .map(|(key,_)| key)
+}
+
+//fn generate_reset_token(username: &[u8]) -> Vec<u8> {
+//}
+
 fn main() {
     let seed = random::<u16>();
-    let encrypted = mt_encrypt(seed, b"HELLLO WORLD");
-    let decrypted = mt_encrypt(seed, &encrypted);
-    println!("Enc: {:?}", encrypted);
-    println!("Enc: {:?}", to_string(&decrypted));
+
+    println!("Prefix size is {}", PREFIX.len());
+    println!("Seed is {}", seed);
+
+    let input = [b'A'; 14];
+    let encrypted = mt_encrypt(seed, &create_input(&input));
+
+    let cracked_seed = crack_seed(&encrypted, &input);
+    println!("seed/cracked_seed: {}:{:?} Same? {}",
+             seed,
+             cracked_seed,
+             Some(seed) == cracked_seed);
 }
