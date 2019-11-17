8417a4551b9c574ff359c310d92cf2dd4cd5f23a
Guy Hershenbaum <hershi@fb.com>
2019-11-17 15:23:09 -0800

    Ex31 oracle

--- ex_31_hmac_sha1_timing_leak/src/main.rs
+++ ex_31_hmac_sha1_timing_leak/src/main.rs
@@ -1,5 +1,30 @@
+#[macro_use]
+extern crate lazy_static;
+
+use rand::prelude::*;
+use std::iter;
+use utils::*;
+use utils::hash_utils::*;
 use utils::hmac::*;
 
+
+const DATA : &[u8] = b"The quick brown fox jumped over the lazy dog";
+const KEY_SIZE : usize = 20;
+
+lazy_static! {
+    pub static ref KEY: Vec<u8> = random_buffer(KEY_SIZE);
+}
+
+fn validate_mac(data: &[u8], hmac: &[u8]) -> bool {
+    let expected = hmac_sha1(&KEY, data);
+    hmac.iter()
+        .zip(expected.iter())
+        .all(|(a,b)| a == b)
+}
+
 fn main() {
-    println!("hmac_sha1('key', 'abc'): {:?}", hmac_sha1(b"key", b"abc"));
+    let correct = hmac_sha1(&KEY, &DATA);
+    let incorrect = hmac_sha1(&KEY, b"hello world");
+    assert!(validate_mac(&DATA, &correct));
+    assert!(!validate_mac(&DATA, &incorrect));
 }
