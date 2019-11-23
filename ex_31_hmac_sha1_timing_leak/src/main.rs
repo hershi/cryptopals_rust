3f9a39ebbd93e12d90ba4cb5f6cdf8271c0fa96c
Guy Hershenbaum <hershi@fb.com>
2019-11-23 11:22:36 -0800

    Multithreaded solution with reasonable perf

--- ex_31_hmac_sha1_timing_leak/src/main.rs
+++ ex_31_hmac_sha1_timing_leak/src/main.rs
@@ -46,6 +46,15 @@ fn find_hmac_length(data: &[u8]) -> usize {
         .unwrap()
 }
 
+fn validate_last_byte(data: &[u8], prefix: &[u8]) -> std::time::Duration {
+    let time = SystemTime::now();
+    if validate_mac(&data, &prefix) {
+        time.elapsed().unwrap()
+    } else {
+        std::time::Duration::from_millis(0)
+    }
+}
+
 fn time_byte_validation(data: &[u8], prefix: &[u8]) -> std::time::Duration {
     let time = SystemTime::now();
     validate_mac(&data, &prefix);
@@ -58,13 +67,19 @@ fn crack_next_byte_concurrent(data: &[u8], prefix: &mut Vec<u8>, pos: usize) {
     let (tx, rx) = mpsc::channel();
     let threads = (0..std::u8::MAX)
         .map(|i| {
+            let is_last_byte = pos == prefix.len() - 1;
             let mut prefix = prefix.clone();
             let data = data.to_vec();
             let tx = mpsc::Sender::clone(&tx);
 
             thread::spawn(move || {
                 prefix[pos] = i;
-                let result = (i, time_byte_validation(&data, &prefix));
+                let time = if is_last_byte {
+                    validate_last_byte(&data, &prefix)
+                } else {
+                    time_byte_validation(&data, &prefix)
+                };
+                let result = (i, time);
                 tx.send(result).unwrap(); }) })
         .collect::<Vec<_>>();
 
@@ -90,17 +105,13 @@ fn find_hmac(data: &[u8]) -> Vec<u8> {
     println!("HMAC length is {}", len);
 
     let mut hmac = vec![0; len];
-    for i in 0..len-1 {
+    for i in 0..len {
+        let time = SystemTime::now();
         crack_next_byte_concurrent(data, &mut hmac, i);
         //crack_next_byte(data, &mut hmac, i);
-        println!("{}", hmac[i]);
+        println!("{}, {:?}", hmac[i], time.elapsed());
     }
     println!("");
-
-    for b in 0..std::u8::MAX {
-        hmac[len-1] = b;
-        if validate_mac(data, &hmac) { return hmac; }
-    }
 
     return hmac
 }
