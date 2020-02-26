836f94823309924fa5b78a54c71eaa4c7b962429
Guy Hershenbaum <hershi@fb.com>
2020-02-26 10:53:34 -0800

    Extract get_nist_prim

--- utils/src/diffie_hellman.rs
+++ utils/src/diffie_hellman.rs
@@ -3,9 +3,13 @@ extern crate num_bigint as bigint;
 
 use bigint::{BigInt, ToBigInt, RandBigInt};
 
+pub fn get_nist_prime() -> BigInt {
+    BigInt::parse_bytes(b"ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff", 32).unwrap()
+}
+
 pub fn generate_pg() -> (BigInt, BigInt) {
-        (BigInt::parse_bytes(b"ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff", 32).unwrap(),
-        2.to_bigint().unwrap())
+    (get_nist_prime(),
+    2.to_bigint().unwrap())
 }
 
 pub fn generate_private_public(p: &BigInt, g: &BigInt) -> (BigInt, BigInt) {
