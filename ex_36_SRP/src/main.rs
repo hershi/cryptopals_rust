75d285082347e0112d3b0c67bb8862e097020e5a
Guy Hershenbaum <hershi@fb.com>
2020-02-29 13:24:52 -0800

    Ex36: ClientHello sent/recv

--- ex_36_SRP/src/main.rs
+++ ex_36_SRP/src/main.rs
@@ -2,10 +2,12 @@ extern crate rand;
 extern crate num_bigint as bigint;
 
 use bigint::{BigInt, Sign, ToBigInt};
+use hmac::*;
 use rand::prelude::*;
 use sha2::*;
 use std::sync::mpsc::{Sender, Receiver, channel};
 use std::thread;
+use serde::{Serialize, Deserialize};
 use utils::diffie_hellman::*;
 
 const G : i32 = 2;
@@ -40,18 +42,55 @@ fn gen_h264_int(inputs: Vec<&[u8]>) -> BigInt {
 
     let x_h = hasher.result();
     //println!("xH: {:?}", x_h);
-    BigInt::from_signed_bytes_le(x_h.as_slice())
+    BigInt::from_bytes_le(Sign::Plus, x_h.as_slice())
 }
 
-fn client(to_server: Sender<Vec<u8>>, from_server: Receiver<Vec<u8>>) {
+#[derive(Serialize, Deserialize, Debug)]
+struct ClientHello {
+    email: String,
+    public_key: String,
 }
 
-fn server(to_client: Sender<Vec<u8>>, from_client: Receiver<Vec<u8>>) {
+#[derive(Serialize, Deserialize, Debug)]
+struct ServerChallenge {
+    salt: i32,
+    public_key: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ClientResponse {
+    resp: Vec<u8>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ServerOk {
+    ok: bool,
+}
+
+fn client(to_server: Sender<String>, from_server: Receiver<String>) {
+    let g = G.to_bigint().unwrap();
+    let (private, public) = generate_private_public(&get_nist_prime(), &g);
+    let hello = ClientHello{
+        email: I.to_string(),
+        public_key: public.to_str_radix(16),};
+
+    println!("\tClient: public key: {:?}", public);
+    let hello_msg = serde_json::to_string(&hello).unwrap();
+    to_server.send(hello_msg).unwrap();
+}
+
+fn server(to_client: Sender<String>, from_client: Receiver<String>) {
     let state = server_init();
-    println!("Server init state: {:?}", state);
+    //println!("Server init state: {:?}", state);
+    println!("Server: Waiting for ClientHello...");
+    let client_hello = &from_client.recv().unwrap();
+    let client_hello: ClientHello = serde_json::from_str(&client_hello).unwrap();
+    let client_public = BigInt::parse_bytes(client_hello.public_key.as_bytes(), 16).unwrap();
+
+    println!("Server: Client public key: {:?}", client_public);
 }
 
-fn server_init() -> (i32, BigInt) {
+fn server_init() -> ServerChallenge {
     let mut rng = thread_rng();
     let salt = rng.gen::<i32>();
     //println!("Server Salt: {}", salt);
@@ -64,5 +103,8 @@ fn server_init() -> (i32, BigInt) {
     let v = g.modpow(&x, &n);
 
     //println!("Server v: {}" , v);
-    (salt, v)
+    ServerChallenge{
+        salt,
+        public_key:  v.to_str_radix(16),
+    }
 }
